74531ad5d56e99e2a72d738fe8bc4105df4bb334
Mert Aktaş <mertaktas231@gmail.com>
2021-06-30 23:04:20 +0300

    Find differs numbers

--- app.js
+++ app.js
@@ -18,3 +18,21 @@ function findOdd(numbers) {
       }
     }
 }
+
+// Find out which one of the given numbers differs from the others
+function iqTest(numbers){
+    var a=[];
+    var e =[];
+    var o =[]
+    a=numbers.split(" ");
+    for(var i=0;i<a.length;i++)
+    {
+    if(a[i]%2==0){ e.push(i)}
+    else
+    o.push(i);
+    }
+    if(e.length>1)
+    return o[0]+1;
+    
+    return e[0]+1;
+}
