00fe05a8349102a84c61d4241ef24a581560ae02
Mert Aktaş <mertaktas231@gmail.com>
2021-06-30 23:00:44 +0300

    Find The Odd İnt

--- app.js
+++ app.js
@@ -3,3 +3,18 @@ function multiply(a, b){
     let result = a * b
     return result
 }
+
+// Find The Odd İnt
+function findOdd(numbers) {
+    var count = 0;
+    for(var i = 0; i < numbers.length; i++){
+      for(var j = 0; j < numbers.length; j++){
+        if(numbers[i] == numbers[j]){
+          count++;
+        }
+      }
+      if(count % 2 != 0){
+        return numbers[i]
+      }
+    }
+}
