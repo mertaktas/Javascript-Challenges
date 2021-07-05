8fa57d6bcf38c037fdef0e997a61738bd7acfbf8
Mert Aktaş <mertaktas231@gmail.com>
2021-07-05 15:27:50 +0300

    A square of squares -> Solution Change

--- app.js
+++ app.js
@@ -503,7 +503,7 @@ function duplicateEncode(word){
 -------Description-------
 Given an integral number, determine if it's a square number.
 */
-var isSquare = function(n){
-  return Number.isInteger(Math.sqrt(n))
+function isSquare(n) {
+  return Math.sqrt(n) % 1 === 0;
 }
 // console.log(isSquare(3));
