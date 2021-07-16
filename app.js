af539b8c592cf57ad2174b2f8b65ccc1da7e2997
Mert Aktaş <mertaktas231@gmail.com>
2021-07-16 15:50:31 +0300

    Problem 26 -> Fizz Buzz

--- app.js
+++ app.js
@@ -517,3 +517,30 @@ var solution = function(firstArray, secondArray) {
 }
 // console.log(solution([1,2,3],[4,5,6])); // 9
 // console.log(solution([10,20,10,2],[10,25,5,-2])); //16.5
+
+//***********************************************
+/* Problem 26 -> Fizz Buzz
+-------Description-------
+If the value is a multiple of 3: use the value "Fizz" instead
+If the value is a multiple of 5: use the value "Buzz" instead
+If the value is a multiple of 3 & 5: use the value "FizzBuzz" instead
+*/
+function fizzbuzz(n){
+  let arr = [];
+  for (let i = 1; i <= n; i++) {
+    if(i % 15 == 0) {
+      arr.push('FizzBuzz')
+    }
+    else if (i % 3 == 0) {
+      arr.push('Fizz')
+    }
+    else if (i % 5 == 0) {
+      arr.push('Buzz')
+    }
+    else {
+      arr.push(i)
+    }
+  }
+  return arr
+}
+// console.log(fizzbuzz(15));
