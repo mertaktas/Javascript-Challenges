bd0f382ed7dbedef0247c161aff1df98e1db219d
Mert Aktaş <mertaktas231@gmail.com>
2021-08-01 16:33:25 +0300

    new problem

--- app.js
+++ app.js
@@ -655,5 +655,25 @@ var defangIPaddr = function (address) {
   }
   return str;
 };
-console.log(defangIPaddr("1.1.1.1"))
+// console.log(defangIPaddr("1.1.1.1"))
+
+/* How Many Numbers Are Smaller Than the Current Number
+Input: nums = [8,1,2,2,3]
+Output: [4,0,1,1,3]
+Explanation: 
+For nums[0]=8 there exist four smaller numbers than it (1, 2, 2 and 3). 
+For nums[1]=1 does not exist any smaller number than it.
+For nums[2]=2 there exist one smaller number than it (1). 
+For nums[3]=2 there exist one smaller number than it (1). 
+For nums[4]=3 there exist three smaller numbers than it (1, 2 and 2).
+*/
+var smallerNumbersThanCurrent = function(nums) {
+  const sortedArr = [...nums].sort((a, b) => a - b)
+  let resultArr = []
+  for (const num of nums) {
+    resultArr.push(sortedArr.indexOf(num))
+  }
+  return resultArr
+};
+console.log(smallerNumbersThanCurrent([8,1,2,2,3]))
 
