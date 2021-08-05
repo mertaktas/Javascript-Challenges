775bf0f3723ca733470e6a06e25ca60844057e6f
Mert Aktaş <mertaktas231@gmail.com>
2021-08-06 00:46:05 +0300

    Problem - Running Sum of 1d Array

--- app.js
+++ app.js
@@ -710,4 +710,27 @@ var getConcatenation = function(nums) {
     return nums.concat(nums)
 };
 
-console.log(getConcatenation([1,2,1]));
+// console.log(getConcatenation([1,2,1]));
+
+/**
+ * Problem - Running Sum of 1d Array
+ * Given an array nums. We define a running 
+ * sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+ * Return the running sum of nums.
+ * 
+ * Input: nums = [1,2,3,4]
+ * Output: [1,3,6,10]
+ * 
+ * 
+*/
+var runningSum = function (nums) {
+  let arr = []
+  let leftNum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    arr.push(nums[i] + leftNum)
+    leftNum += nums[i]
+  }
+  return arr
+};
+
+console.log(runningSum([3,1,2,10,1]));
