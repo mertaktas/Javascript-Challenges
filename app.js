f59d6588cdda0348b64e8aaa06eaffe1500adda2
Mert Aktaş <mertaktas231@gmail.com>
2021-08-06 16:51:00 +0300

    Problem - Number of Good Pairs

--- app.js
+++ app.js
@@ -767,4 +767,26 @@ var kidsWithCandies = function (candies, extraCandies) {
   candies.map(candie=> candie + extraCandies >= Math.max(...candies) ? result.push(true) : result.push(false))
   return result
 };
-console.log(kidsWithCandies([4,2,1,1,2],1))
+// console.log(kidsWithCandies([4, 2, 1, 1, 2], 1))
+
+/** Problem - Number of Good Pairs
+ * Input: nums = [1,2,3,1,1,3]
+ * Output: 4
+Explanation: There are 4 good pairs 
+(0,3), (0,4), (3,4), (2,5) 0-indexed.
+ * 
+*/
+
+function numIdenticalPairs(numbers) {
+  const counts = {};
+  numbers.map(elem => {
+    counts[elem] = counts[elem] ? counts[elem] + 1 : 1;
+  })
+  const result = Object.values(counts).reduce((acc, value) => {
+    return acc += (value*(value-1))/2
+  }, 0)
+  return result
+  
+}
+
+console.log(numIdenticalPairs([1,2,3,1,1,3]));
