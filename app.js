2255de45bc970762362c79048c2c2f7679504ec6
Mert Aktaş <mertaktas231@gmail.com>
2021-08-10 00:21:50 +0300

    Problem - Find Center of Star Graph

--- app.js
+++ app.js
@@ -964,5 +964,30 @@ var balancedStringSplit = function(s) {
   },0)
 };
 
-console.log(balancedStringSplit("RLLLLRRRLR"));
+// console.log(balancedStringSplit("RLLLLRRRLR"));
+
+/*** Problem - Find Center of Star Graph
+ * 
+ * There is an undirected star graph consisting of n nodes labeled from 1 to n. 
+ * A star graph is a graph where there is one center node and exactly n - 1 edges 
+ * that connect the center node with every other node.
+ * You are given a 2D integer array edges where each edges[i] = [ui, vi] indicates 
+ * that there is an edge between the nodes ui and vi. Return the center of the given star graph.
+ * 
+ */
+var findCenter = function(edges) {
+  let a = edges[0][0]
+  let b = edges[0][1]
+  let c = edges[1][0]
+  let d = edges[1][1]
+
+  if(a == c || b == c){
+    return c
+  }else {
+    return d
+  }
+};
+let edges = [[1,2],[2,3],[4,2]]
+console.log(findCenter(edges));
+
 
