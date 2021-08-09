ccaf2ecd619fb677bc5bde9d43ead129d0a5ccea
Mert Aktaş <mertaktas231@gmail.com>
2021-08-09 16:39:44 +0300

    Problem - Count Items Matching a Rule

--- app.js
+++ app.js
@@ -923,5 +923,28 @@ var interpret = function(command) {
   return result.join('');
 };
 
-console.log(interpret("G()(al)"));
+// console.log(interpret("G()(al)"));
+
+/*** Problem - Count Items Matching a Rule
+ * You are given an array items, where each items[i] = [typei, colori, namei] 
+ * describes the type, color, and name of the ith item. 
+ * You are also given a rule represented by two strings, ruleKey and ruleValue.
+ * The ith item is said to match the rule if one of the following is true:
+
+ruleKey == "type" and ruleValue == type.
+ruleKey == "color" and ruleValue == color.
+ruleKey == "name" and ruleValue == name. 
+ * 
+ * 
+ */
+var countMatches = function(items, ruleKey, ruleValue) {
+  const ruleKeys = ["type","color","name"]
+  const idx = ruleKeys.findIndex(key => key === ruleKey);
+  let result = items.reduce((acc,val)=>{
+    return acc += val[idx] == ruleValue ? +1 : 0
+  },0)
+  return result
+};
+
+console.log(countMatches([["phone","blue","pixel"],["computer","silver","lenovo"],["phone","gold","iphone"]], ruleKey = "color", ruleValue = "silver"));
 
