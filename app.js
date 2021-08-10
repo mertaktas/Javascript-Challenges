b5fcd316fa5cd6cc87e5d1e584a768b8236beac2
Mert Aktaş <mertaktas231@gmail.com>
2021-08-10 16:47:18 +0300

    Problem -Decompress Run-Length Encoded List

--- app.js
+++ app.js
@@ -1093,4 +1093,27 @@ var minOperations = function(boxes) {
     return result
 };
 
-console.log(minOperations(boxes = "001011"));
+// console.log(minOperations(boxes = "001011"));
+
+/*** Problem -Decompress Run-Length Encoded List
+ * 
+ * We are given a list nums of integers representing a list compressed with run-length encoding.
+ * Consider each adjacent pair of elements [freq, val] = [nums[2*i], nums[2*i+1]] (with i >= 0).  
+ * For each such pair, there are freq elements with value val concatenated in a sublist. ,
+ * Concatenate all the sublists from left to right to generate the decompressed list.
+ * Return the decompressed list.
+ * 
+ */
+var decompressRLElist = function(nums) {
+    let result = []
+    nums.map((num,idx)=>{
+      if(idx%2 == 0){
+        for (let i = 1; i <= num; i++) {
+          result.push(nums[idx+1])
+        }
+      }
+    })
+    return result
+};
+
+console.log(decompressRLElist([1,2,3,4]));
