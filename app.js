8d3c7bcd3bc9938c92ea5badc36822427dfc85b9
Mert Aktaş <mertaktas231@gmail.com>
2021-08-10 15:29:46 +0300

    Problem - Minimum Number Of Deci-Binary Numbers

--- app.js
+++ app.js
@@ -1031,4 +1031,16 @@ var maxProductDifference = function(nums) {
     return (arr[arr.length-1]*arr[arr.length-2])-(arr[0]*arr[1])
 };
 
-console.log(maxProductDifference(nums = [4,2,5,9,7,4,8]));
+// console.log(maxProductDifference(nums = [4,2,5,9,7,4,8]));
+
+/**** Problem - Partitioning Into Minimum Number Of Deci-Binary Numbers
+ * 
+ * A decimal number is called deci-binary if each of its digits is either 0 or 1 without 
+ * any leading zeros. For example, 101 and 1100 are deci-binary, while 112 and 3001 are not.
+ * Given a string n that represents a positive decimal integer, 
+ * return the minimum number of positive deci-binary numbers needed so that they sum up to n.
+ * 
+ */
+const minPartitions = n => Math.max(...n.split(''))
+
+console.log(minPartitions(n = "82734"))
