0c2c61ce0bdb5579ccefe501a581321b4c3b246e
mertaktas <mertaktas231@gmail.com>
2021-08-17 19:35:03 +0300

    Problem - Check if the Sentence Is Pangram

--- app.js
+++ app.js
@@ -1236,4 +1236,23 @@ var processQueries = function(queries, m) {
   return result
 };
 
-console.log(processQueries(queries = [7,5,5,8,3],8))
+// console.log(processQueries(queries = [7,5,5,8,3],8))
+
+/**** Problem - Check if the Sentence Is Pangram
+ * 
+ * A pangram is a sentence where every letter of the English alphabet appears at least once.
+ * Given a string sentence containing only lowercase English letters, 
+ * return true if sentence is a pangram, or false otherwise.
+ * 
+ */
+var checkIfPangram = function(sentence) {
+  const str = new Set(sentence.split('').sort())
+  let count = 0
+  for (const s of str) {
+    count++
+  }
+  return count == 26
+};
+
+let sentence = "thequickbrownfoxjumpsoverthelazydog"
+console.log(checkIfPangram(sentence))
