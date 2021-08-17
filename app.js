c7ebfde6e2e0877a5b4966160cf97dcf4c0496b0
mertaktas <mertaktas231@gmail.com>
2021-08-17 20:27:10 +0300

    Problem - Check If Two String are Equivalent

--- app.js
+++ app.js
@@ -1282,4 +1282,19 @@ var numberOfMatches = function(n) {
     return result
 };
 
-console.log(numberOfMatches(14));
+// console.log(numberOfMatches(14));
+
+/*** Problem - Check If Two String Arrays are Equivalent
+ * 
+ * Given two string arrays word1 and word2, return true if the two arrays represent 
+ * the same string, and false otherwise.
+ * A string is represented by an array if the array elements concatenated in order forms the string.
+ * 
+ */
+
+var arrayStringsAreEqual = function(word1, word2) {
+    return word1.join('') == word2.join('')
+};
+
+let word1 = ["ab", "c"], word2 = ["a", "bc"]
+console.log(arrayStringsAreEqual(word1,word2));
